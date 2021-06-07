cf7365dee4abf071c1717dda4dea71ab209311e3
Vinycius <vinycius.c.rezende@accenture.com>
2021-06-07 17:39:15 -0300

    criar pacotes e classes

--- src/main/java/com/projetosemana3/Projetosemana3Application.java
+++ src/main/java/com/projetosemana3/Projetosemana3Application.java
@@ -1,9 +1,21 @@
 package com.projetosemana3;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.projetosemana3"})
+@ComponentScan(basePackages = {"com.projetosemana3.*"})
+@EnableJpaRepositories(basePackages = {"com.accenture.projetosemana3.repository"})
+@EnableTransactionManagement
+@RestController
+@EnableAutoConfiguration
 public class Projetosemana3Application {
 
 	public static void main(String[] args) {
