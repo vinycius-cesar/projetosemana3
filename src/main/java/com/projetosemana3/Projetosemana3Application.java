85eb5675a3a41ed98e1f98bba2201108b8789f3a
Vinycius <vinycius.c.rezende@accenture.com>
2021-06-07 17:31:20 -0300

    iniciando o projeto

---
+++ src/main/java/com/projetosemana3/Projetosemana3Application.java
@@ -0,0 +1,14 @@
+package com.projetosemana3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetosemana3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetosemana3Application.class, args);
+		System.out.println("hello wooooooooooooorld, agora rodou em. bora, pracima meu mano");
+	}
+
+}
---
+++ src/test/java/com/projetosemana3/Projetosemana3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetosemana3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetosemana3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
