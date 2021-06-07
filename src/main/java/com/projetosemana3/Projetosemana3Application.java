08347fc5a6515630cbf6a829200e83d16684f0b1
Vinycius <vinycius.c.rezende@accenture.com>
2021-06-07 18:02:19 -0300

    classe model usuario criada

--- src/main/java/com/projetosemana3/Projetosemana3Application.java
+++ src/main/java/com/projetosemana3/Projetosemana3Application.java
@@ -20,7 +20,7 @@ public class Projetosemana3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Projetosemana3Application.class, args);
-		System.out.println("hello wooooooooooooorld, agora rodou em. bora, pracima meu mano");
+		
 	}
 
 }
